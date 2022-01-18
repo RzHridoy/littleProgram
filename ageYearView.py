fc979acf7e95363497cb25fa8e063458a3a3b942
RzHridoy <59144225+RzHridoy@users.noreply.github.com>
2022-01-19 01:38:44 +0600

    Add files via upload

---
+++ ageYearView.py
@@ -0,0 +1,11 @@
+print('What is your name?')
+name = input()
+
+print(f'''Hi there, {name}!
+Input your age please...''')
+
+age = int(input())
+birth_year = 2022 - age
+
+print(f'''Your age is {age} and birth year is {str(birth_year)}.''')
+print('Thanks for your Information!!!')
